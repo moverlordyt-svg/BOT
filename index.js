d08c9f47d6e091e82de871464149272a68b7b448
moverlordyt-svg <moverlordyt@gmail.com>
2026-01-03 17:16:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(process.env.OTg1NDk0NTM4NzI4NjQwNTUy.GjBReb.u6CVEo42x97Jaz_hY4UXBAz0d_OyDOORh55ujM);
+client.login(process.env.TOKEN);
